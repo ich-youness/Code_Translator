b5a9c6a3a3e0469d00c3179ac89c2f106b74f159
PC <youness.ichen@usmba.ac.ma>
2023-01-22 22:18:53 +0100

    update the server url

--- client/code-trans.js
+++ client/code-trans.js
@@ -62,7 +62,8 @@ const handlesubmit = async(e) => {
   if(response.ok){//response
    
     const datat = await response.json();
-    const da =  datat.bot.trim();
+    const da = undefined;
+     da =  datat.bot.trim();
     // console.log("reponse dyal server "+da);
     reponseai(typetext(reponseid,da));
    
