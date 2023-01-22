e8d2e91107dd2e0bb0957b34e7867e2c69694319
PC <youness.ichen@usmba.ac.ma>
2023-01-22 15:33:00 +0100

    first commit

---
+++ client/code-trans.js
@@ -0,0 +1,94 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+  const btn= document.getElementById("btn");
+  const form = document.querySelector('form');
+  const reponseid = document.getElementById("xx");
+  
+  // console.log("from "+ lang1 + " to "+lang2);
+  
+
+  
+
+
+
+function typetext(element,text){// type d'ecriture, lettre par lettre
+  let index = 0;
+  element.innerHTML=" ";
+  let interval = setInterval(() => {
+    if(index < text.length ){
+      element.innerHTML+=text.charAt(index);
+      index++;
+    }else{
+      clearInterval(interval);
+    }
+  },20)
+}
+
+
+
+// value is the ai generated message
+function reponseai(value){
+  // return `<div class="message" style="color:white"> ${value} </div> `;
+  return value;
+}
+
+
+const handlesubmit = async(e) => {
+// async function handlesubmit(e){
+  e.preventDefault();// prevent the default behavior of the browser(the form submit will not reload the page)
+  // const data = new FormData(form);
+
+  //bot div to write the answer
+
+  var lang1 = document.getElementById("1").value;
+  var lang2 = document.getElementById("2").value;
+  console.log("from "+ lang1 + " to "+lang2);
+
+  const data = new FormData(form);
+  // console.log("forme data"+data);
+  const response = await fetch("http://localhost:5300",{// sends
+    method: 'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify({
+      prompt: "#"+ lang1 +" to "+ lang2+" :\n"+data.get("prompt")// c'est ca le prompt qu'on doit modifier
+    })
+  })
+  
+ 
+
+  if(response.ok){//response
+    const datat = await response.json();
+    const da = datat.bot.trim();
+    // console.log("reponse dyal server "+da);
+    reponseai(typetext(reponseid,da));
+   
+    // console.log(response.data.choices[0].text);
+    // reponseid.innerHTML = reponseai(da+ " daata ");
+    //  console.log("la reponse:"+ reponseai(da));
+  }
+  else{
+    console.log("wrong here");
+  }
+
+}
+
+
+
+/// fonction to fetch the info from the server at the port 5300 (this one is taken from the api doc)
+
+  
+
+
+form.addEventListener('submit', handlesubmit);
+
+
+// console.log(reponseid);
+
+
+
+
+})
+// npm run dev
+//  npm run server
---
+++ server/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi} from 'openai';
+
+dotenv.config();
+console.log(process.env.OPENAI_API_KEY);
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', async(req, res) => {
+    res.status(200).send({
+        message: 'Helloo from my bedroom',
+    })
+});
+
+app.post('/', async(req, res) =>{
+    try{
+        const prompt = req.body.prompt;// kt9raha
+        // console.log(prompt);
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            // prompt: `${prompt}`,// here is where you wrte to the chatgpt api
+            "prompt": prompt,
+            temperature: 0.7,
+            max_tokens: 1009,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+          });
+          res.send({
+           
+            bot:  response.data.choices[0].text
+            
+          })
+       
+        console.log(response);
+
+
+          console.log("3tat rep "+ response.data.choices[0].text);
+        
+        // console.log("fef");
+    }catch(error){
+        console.log(error);
+        res.send({error})
+    }
+})
+app.listen(5300, () => console.log('Server is running on http://localhost:5300'));
